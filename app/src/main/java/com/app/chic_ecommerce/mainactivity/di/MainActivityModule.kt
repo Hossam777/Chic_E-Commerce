50f80dce6011afa6c7255b7da315a169420b76c5
Hossam777 <31036061+Hossam777@users.noreply.github.com>
2021-08-01 12:48:31 +0200

    Initial commit

---
+++ app/src/main/java/com/app/chic_ecommerce/mainactivity/di/MainActivityModule.kt
@@ -0,0 +1,7 @@
+package com.app.chic_ecommerce.mainactivity.di
+
+import org.kodein.di.Kodein
+
+val mainActivityModule = Kodein.Module("MainActivityModule"){
+
+}
---
+++ app/src/main/java/com/app/chic_ecommerce/mainactivity/presentation/MainActivityViewModel.kt
@@ -0,0 +1,7 @@
+package com.app.chic_ecommerce.mainactivity.presentation
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+
+}
